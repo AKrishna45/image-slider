d05a9ff3d698b2bd58a75bef00c7fe7cb2d3b243
AKrishna45 <ukdkittu@gmail.com>
2025-08-29 21:39:48 -0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
-let flag=0;
-function controller (x) {
-  flag = flag +x;
-  slideshow(flag);
+let slideIndex = 0;
+showSlide(slideIndex);
 
+function controller(n) {
+  showSlide(slideIndex += n);
 }
-slideshow(flag);
-function slideshow(num) {
-let slides=document.getElementsByClassName('slide');
-   
-if(num == slides.length) {
-  flag=0;
-  num=0;
+
+function showSlide(n) {
+  const slides = document.getElementsByClassName("slide");
+  if (n >= slides.length) slideIndex = 0;
+  if (n < 0) slideIndex = slides.length - 1;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex].style.display = "block";
 }
-if(num < 0) {
-  flag = slides.length-1;
-  num= slides.length-1;
-}
-for(let y of slides) {
-  y.style.display ="none";
-}
-slides[num].style.display="block";
-}
+setInterval(() => {
+  controller(1);
+}, 3000);
+
